a8860b612bf5334bb9420a021f157e6f0eee9061
西聖勇弥 <seiya1@nisiseiya.local>
2016-03-28 14:32:42 +0900

    create_action_routeSetting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
  get 'questions'          =>  'questions#index'
  get 'questions/new'      =>  'questions#new'
+ get 'tweets'             =>  'questions#create'
 end
