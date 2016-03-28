64f827558dd2c971e0e128f6f36e049179424e48
西聖勇弥 <seiya1@nisiseiya.local>
2016-03-28 15:51:13 +0900

    root_setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'questions#'
+  root 'questions#index'
   get    'questions'          =>  'questions#index'
   get    'questions/new'      =>  'questions#new'
   post   'questions'          =>  'questions#create'
