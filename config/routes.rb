c03f66a659543959c0ff6c61db9ffe3647c2c263
西聖勇弥 <seiya1@nisiseiya.local>
2016-03-28 14:33:34 +0900

    questions_pass_change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  get 'questions'          =>  'questions#index'
  get 'questions/new'      =>  'questions#new'
- get 'tweets'             =>  'questions#create'
+ get 'questions'             =>  'questions#create'
 end
