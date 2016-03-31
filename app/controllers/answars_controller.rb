94fc8513f3e78df58c82fac6068dfd9b04cc0c29
西聖勇弥 <seiya1@nisiseiya.local>
2016-03-31 17:58:03 +0900

    strong_params_setting

--- app/controllers/answars_controller.rb
+++ app/controllers/answars_controller.rb
@@ -1,5 +1,10 @@
 class AnswarsController < ApplicationController
   def create
-    Answar.create(text: params[:text], question_id: params[:question_id], user_id: current_user.id)
+    Answar.create(text: answar_params[:text], question_id: answar_params[:question_id], user_id: current_user.id)
+  end
+
+  private
+  def answar_params
+    params.permit(:text, :question_id)
   end
 end
