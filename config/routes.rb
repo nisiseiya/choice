44c9d255e722fc3f25e78e90aa6b5efca4510998
西聖勇弥 <seiya1@nisiseiya.local>
2016-03-31 17:36:37 +0900

    resources_setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'questions#index'
-  get    'questions'          =>  'questions#index'
-  get    'questions/new'      =>  'questions#new'
-  post   'questions'          =>  'questions#create'
-  get    'users/:id'          =>  'users#show'
-  delete 'questions/:id'      =>  'questions#destroy'
-  get    'questions/:id/edit' =>  'questions#edit'
-  patch  'questions/:id'      =>  'questions#update'
-  get    'questions/:id'      =>  'questions#show'
+  resources  :questions
+  resources  :users, only: [:show]
 end
