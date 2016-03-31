dbef728dbf44526a6cfa9ea45311076ba27303fd
西聖勇弥 <seiya1@nisiseiya.local>
2016-03-31 15:14:52 +0900

    show_action_setting

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -31,6 +31,10 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   private
   def question_params
     params.permit(:text)
